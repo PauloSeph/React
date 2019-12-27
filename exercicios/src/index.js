1b9bbc23ec79611a579a2c100b3cfa036eb82435
Seph <paulo.seph@hotmail.com>
2019-12-27 16:33:42 -0300

    END

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Multi, { BoaTarde } from './componentes/Multiplos'
-
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render( 
-
 <div>
-<BoaTarde nome="Alfredo"/>
-<Multi.BoaNoite nome="Ana"/> 
+<Pai nome="Paulo" sobrenome="Silva"/>
+    <Filho nome="Pedro" />
+    <Filho nome="Paulo" />
+    <Filho nome="Carla" />
 </div>
 
 , document.getElementById('root'))

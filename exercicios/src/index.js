889f78a0a48669d02ff29c96e9f77bef884793e7
Seph <paulo.seph@hotmail.com>
2019-12-27 14:53:06 -0300

    Multiplos componentes

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const elemento = <h1>React 2</h1>
-
-ReactDOM.render( elemento, document.getElementById('root'))
+import Multi, { BoaTarde } from './componentes/Multiplos'
 
 
+ReactDOM.render( 
+
+<div>
+<BoaTarde nome="Alfredo"/>
+<Multi.BoaNoite nome="Ana"/> 
+</div>
+
+, document.getElementById('root'))
+
+
+ 
